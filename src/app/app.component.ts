10b814b96244c48325c2bf9f30dc07b9033fd070
Rodrigo <rokxfull@gmail.com>
2024-06-17 19:52:36 -0300

    commit inicio

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Equipe', url: '/equipe/equipes' },
+    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' }
+  ];
+  public labels = ['Labels'];
+  constructor() {}
+}
